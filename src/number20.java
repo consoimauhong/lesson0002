882e85d9733d390b8f7e07bc13f1dd1b5ab126e9
Your Name <you@example.com>
2022-12-06 14:56:36 +0700

    neko

---
+++ src/number20.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class number20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số vào: ");
+        int number = scanner.nextInt();
+        int count = 0;
+        if (number < 2) {
+            System.out.println(number + "khong phai là so nguyen ");
+        } else {
+
+            for (int i = 2; count < number; i++) {
+                int n = 2;
+                boolean check = true;
+                while (n < i) {
+                    if (i % n == 0) {
+                        check = false;
+                        break;
+                    }
+                    n++;
+                }
+                if (check){
+                    System.out.println(i);
+//                    System.out.println(number + "la so nguyen to");
+                    count++;
+
+                 }
+        }
+
+            }
+        }
+    }
+
